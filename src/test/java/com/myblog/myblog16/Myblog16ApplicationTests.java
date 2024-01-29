302f759cd26ed9b18584eac5b61e11ff33c85a56
adarsh <adarshbeck6985@gmail.com>
2024-01-29 08:07:02 +0530

    create post for myblog16

---
+++ src/test/java/com/myblog/myblog16/Myblog16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
